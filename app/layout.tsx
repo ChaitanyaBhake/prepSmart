183807d2e90a02ada9798ccc107294f89d0677c9
Chaitanya Bhake <chaitubhake2013@gmail.com>
2025-04-18 17:13:27 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const monoSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "PrepSmart AI",
+  description: "An AI-powered platform for preparing mock interview",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monoSans.className} antialiased pattern`}
+      >
+        {children}
+
+        <Toaster/>
+      </body>
+    </html>
+  );
+}
